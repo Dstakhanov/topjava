287c22dc9a4154a21749fe914a32cf0670e950a3
Dmitriy <dimitrium2011@gmail.ru>
2019-04-09 23:41:34 +0300

    8 10 add rest security

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,41 @@
+
+$(function () {
+    makeEditable({
+        ajaxUrl: mealUrl,
+        datatableOpts: {
+            "columns": [
+                {
+                    "data": "dateTime"
+                },
+                {
+                    "data": "description"
+                },
+                {
+                    "data": "calories"
+                },
+                {
+                    "render": renderEditBtn,
+                    "defaultContent": "",
+                    "orderable": false
+                },
+                {
+                    "render": renderDeleteBtn,
+                    "defaultContent": "",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [
+                    0,
+                    "desc"
+                ]
+            ],
+            "createdRow": function (row, data, dataIndex) {
+                $(row).attr("data-mealExcess", data.excess);
+            },
+        },
+        updateTable: updateFilteredTable
+    });
+
+    $.datetimepicker.setLocale(localeCode);
+});
